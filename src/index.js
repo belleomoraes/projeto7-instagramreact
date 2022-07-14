8e668b61c0bfe8087e5c895ddfc474cf83d18434
belleomoraes <iolliveira.moraes@gmail.com>
2022-07-14 20:06:23 -0300

    compartimentos grandes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import ReactDOM from 'react-dom';
+import Navbar from './compartimentos/Navbar';
+import BarraStories from './compartimentos/BarraStories';
+import Posts from './compartimentos/Posts';
+import Sidebar from './compartimentos/Sidebar';
+import FundoMobile from './compartimentos/FundoMobile';
+
+function App() {
+    return (
+        <div>
+        <Navbar />
+        <div class="corpo"> 
+        <div class="esquerda">
+        <BarraStories />
+        <Posts />
+        </div>
+        <Sidebar />
+        <FundoMobile />
+      </div>
+      </div>
+    )
+}
+
+ReactDOM.render(<App />, document.querySelector(".root"));
